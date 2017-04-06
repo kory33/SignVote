cbf9b23181ea48c7d80cb08a15520b11566e7e27
Kory33 <konryoitdv@gmail.com>
2017-04-06 16:00:36 +0200

    add constant files

---
+++ src/main/java/com/github/kory33/signvote/constants/PermissionNodes.java
@@ -0,0 +1,9 @@
+package com.github.kory33.signvote.constants;
+
+public class PermissionNodes {
+    private static final String BASE_NODE = "signvote";
+    
+    public static final String VOTE           = BASE_NODE + "vote";
+    public static final String CREATE_SESSION = BASE_NODE + "createsession";
+    public static final String MODIFY_SESSION = BASE_NODE + "modifysession";    
+}
---
+++ src/main/java/com/github/kory33/signvote/constants/SignTexts.java
@@ -0,0 +1,8 @@
+package com.github.kory33.signvote.constants;
+
+import org.bukkit.ChatColor;
+
+public class SignTexts {
+    public static final String SIGN_CREATION_TEXT   = "[SignVote]";
+    public static final String REGISTERED_SIGN_TEXT = ChatColor.AQUA + "[SignVote]";
+}
