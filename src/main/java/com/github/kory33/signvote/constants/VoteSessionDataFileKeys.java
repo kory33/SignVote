5154151682c3fc7ccbbea4de5b7ae5c8ebfdbd66
Kory33 <konryoitdv@gmail.com>
2017-04-07 23:18:50 +0200

    add file keys constant class

---
+++ src/main/java/com/github/kory33/signvote/constants/VoteSessionDataFileKeys.java
@@ -0,0 +1,7 @@
+package com.github.kory33.signvote.constants;
+
+public class VoteSessionDataFileKeys {
+    public static final String NAME = "name";
+
+    public static final String VOTE_SCORE_LIMITS = "votelimits";
+}
