382079cfe9b3a5729caa5629a54c1699d9c5cb3d
Kory33 <konryoitdv@gmail.com>
2017-04-07 10:04:10 +0200

    add command executor / paths constants

---
+++ src/main/java/com/github/kory33/signvote/command/SignVoteCommandExecutor.java
@@ -0,0 +1,21 @@
+package com.github.kory33.signvote.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.github.kory33.signvote.core.SignVote;
+
+public class SignVoteCommandExecutor implements CommandExecutor{
+    private final SignVote plugin;
+    
+    public SignVoteCommandExecutor(SignVote plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        return false;
+    }
+}
---
+++ src/main/java/com/github/kory33/signvote/constants/DirectoryPaths.java
@@ -0,0 +1,6 @@
+package com.github.kory33.signvote.constants;
+
+public class DirectoryPaths {
+    public static final String SESSION_DIR = "sessions";
+    public static final String VOTE_POINTS_DIR = "votepoints";
+}
