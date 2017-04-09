ad9227606b0be9d453f94adf91ff2553dd1fdfc4
Kory33 <konryoitdv@gmail.com>
2017-04-09 16:33:41 +0200

    move some message nodes / write messages.json

--- src/main/java/com/github/kory33/signvote/constants/MessageConfigurationNodes.java
+++ src/main/java/com/github/kory33/signvote/constants/MessageConfigurationNodes.java
@@ -5,6 +5,9 @@ public class MessageConfigurationNodes {
     public static final String COMMAND_HELP = COMMAND_ROOT + ".help";
     
     public static final String CREATE_COMMAND_HELP      = COMMAND_ROOT + ".create.help";
+    public static final String VOTEPOINT_NAME_EMPTY     = COMMAND_ROOT + ".create.error.vpemptyname";
+    public static final String VOTEPOINT_ALREADY_EXISTS = COMMAND_ROOT + ".create.error.vpexists";
+    
     public static final String ADD_SCORE_COMMAND_HELP   = COMMAND_ROOT + ".addscore.help";
     
     public static final String OPEN_COMMAND_HELP        = COMMAND_ROOT + ".open.help";
@@ -20,13 +23,9 @@ public class MessageConfigurationNodes {
     public static final String REACHED_VOTE_SCORE_LIMIT = COMMAND_ROOT + ".vote.error.reachedlimit";
     public static final String VOTEPOINT_ALREADY_VOTED  = COMMAND_ROOT + ".vote.error.alreadyvoted";
     
-    public static final String ERROR_ROOT = "error";
-    
-    public static final String MISSING_PERMS = ERROR_ROOT + ".missingpermission";
-    
-    public static final String SESSION_DOES_NOT_EXIST   = ERROR_ROOT + ".nosession";
-    public static final String VOTEPOINT_NAME_EMPTY     = ERROR_ROOT + ".vpemptyname";
-    public static final String VOTEPOINT_ALREADY_EXISTS = ERROR_ROOT + ".vpexists";
-    public static final String VOTEPOINT_DOES_NOT_EXIST = ERROR_ROOT + ".novotepoint";
-    public static final String COMMAND_ONLY_FOR_PLAYERS = ERROR_ROOT + ".onlyforplayers";
+    public static final String GENERIC_COMMAND_ERROR    = COMMAND_ROOT + ".generic.error";
+    public static final String COMMAND_ONLY_FOR_PLAYERS = GENERIC_COMMAND_ERROR + ".onlyforplayers";
+    public static final String SESSION_DOES_NOT_EXIST   = GENERIC_COMMAND_ERROR + ".nosession";
+    public static final String VOTEPOINT_DOES_NOT_EXIST = GENERIC_COMMAND_ERROR + ".novotepoint";
+    public static final String MISSING_PERMS            = GENERIC_COMMAND_ERROR + ".missingpermission";
 }
