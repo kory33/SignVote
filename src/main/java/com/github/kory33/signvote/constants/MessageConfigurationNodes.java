6001e29ea5474fd3e6fd0454fc33a8633e504913
Kory33 <konryoitdv@gmail.com>
2017-04-09 15:13:10 +0200

    ammend msg nodes / add empty messages.json

--- src/main/java/com/github/kory33/signvote/constants/MessageConfigurationNodes.java
+++ src/main/java/com/github/kory33/signvote/constants/MessageConfigurationNodes.java
@@ -8,16 +8,17 @@ public class MessageConfigurationNodes {
     public static final String ADD_SCORE_COMMAND_HELP   = COMMAND_ROOT + ".addscore.help";
     
     public static final String OPEN_COMMAND_HELP        = COMMAND_ROOT + ".open.help";
-    public static final String SESSION_ALREADY_OPENED   = COMMAND_ROOT + ".open.alreadyopened";
+    public static final String SESSION_ALREADY_OPENED   = COMMAND_ROOT + ".open.error.alreadyopened";
     
     public static final String CLOSE_COMMAND_HELP       = COMMAND_ROOT + ".close.help";
-    public static final String SESSION_ALREADY_CLOSED   = COMMAND_ROOT + ".close.alreadyclosed";
+    public static final String SESSION_ALREADY_CLOSED   = COMMAND_ROOT + ".close.error.alreadyclosed";
     
     public static final String VOTE_COMMAND_HELP        = COMMAND_ROOT + ".vote.help";
-    public static final String INVALID_VOTE_SCORE       = COMMAND_ROOT + ".vote.invalidscore";
-    public static final String REACHED_VOTE_SCORE_LIMIT = COMMAND_ROOT + ".vote.reachedlimit";
-    public static final String VOTEPOINT_ALREADY_VOTED  = COMMAND_ROOT + ".vote.alreadyvoted";
     public static final String VOTED                    = COMMAND_ROOT + ".vote.voteaccepted";
+    
+    public static final String INVALID_VOTE_SCORE       = COMMAND_ROOT + ".vote.error.invalidscore";
+    public static final String REACHED_VOTE_SCORE_LIMIT = COMMAND_ROOT + ".vote.error.reachedlimit";
+    public static final String VOTEPOINT_ALREADY_VOTED  = COMMAND_ROOT + ".vote.error.alreadyvoted";
     
     public static final String ERROR_ROOT = "error";
     
