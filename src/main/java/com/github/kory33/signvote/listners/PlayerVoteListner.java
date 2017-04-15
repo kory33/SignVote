d1c16a4fe2421d26866c5d00dc858207247dcae7
Kory33 <konryoitdv@gmail.com>
2017-04-15 14:54:40 +0200

    define vote/unvote interface displaying flow(some TODOs are left)

--- src/main/java/com/github/kory33/signvote/listners/PlayerVoteListner.java
+++ src/main/java/com/github/kory33/signvote/listners/PlayerVoteListner.java
@@ -1,19 +1,61 @@
 package com.github.kory33.signvote.listners;
 
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.github.kory33.signvote.configurable.JSONConfiguration;
 import com.github.kory33.signvote.core.SignVote;
+import com.github.kory33.signvote.manager.VoteSessionManager;
+import com.github.kory33.signvote.model.VotePoint;
+import com.github.kory33.signvote.session.VoteSession;
+import com.github.kory33.signvote.ui.PlayerChatInterface;
+import com.github.kory33.signvote.ui.PlayerUnvoteInterface;
+import com.github.kory33.signvote.ui.PlayerVoteInterface;
 
 public class PlayerVoteListner implements Listener {
+    private final VoteSessionManager voteSessionManager;
+    private final JSONConfiguration messageConfig;
+
     public PlayerVoteListner(SignVote plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.voteSessionManager = plugin.getVoteSessionManager();
+        this.messageConfig = plugin.getMessagesConfiguration();
     }
     
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
     public void onPlayerInteractWithVotePoint(PlayerInteractEvent event) {
-        // TODO display the voting interfaces in the players chat using tellraw system
+        BlockState state = event.getClickedBlock().getState();
+        if (!(state instanceof Sign)) {
+            return;
+        }
+        Sign sign = (Sign) state;
+
+        VoteSession session = this.voteSessionManager.getVoteSession(sign);
+        if (session == null) {
+            return;
+        }
+        
+        VotePoint votePoint = session.getVotePoint(sign);
+        if (votePoint == null) {
+            return;
+        }
+        
+        Player clickPlayer = event.getPlayer();
+        
+        PlayerChatInterface chatInterface;
+        if (session.getVoteManager().hasVoted(clickPlayer, votePoint)) {
+            chatInterface = new PlayerUnvoteInterface(clickPlayer, session, votePoint, messageConfig);
+        } else {
+            chatInterface = new PlayerVoteInterface(clickPlayer, session, votePoint, messageConfig);
+        }
+        
+        chatInterface.send();
+        event.setCancelled(true);
+        return;
     }
 }
---
+++ src/main/java/com/github/kory33/signvote/ui/PlayerUnvoteInterface.java
@@ -0,0 +1,29 @@
+package com.github.kory33.signvote.ui;
+
+import org.bukkit.entity.Player;
+
+import com.github.kory33.signvote.configurable.JSONConfiguration;
+import com.github.kory33.signvote.model.VotePoint;
+import com.github.kory33.signvote.session.VoteSession;
+import com.github.ucchyocean.messaging.tellraw.MessageComponent;
+
+public class PlayerUnvoteInterface extends PlayerChatInterface {
+	private final VoteSession session;
+	private final VotePoint votePoint;
+	private final JSONConfiguration messageConfig;
+	
+	public PlayerUnvoteInterface(Player player, VoteSession session, VotePoint votePoint,
+			JSONConfiguration messageConfig) {
+		super(player);
+		
+		this.session = session;
+		this.votePoint = votePoint;
+		this.messageConfig = messageConfig;
+	}
+
+	@Override
+	protected MessageComponent constructInterfaceMessages() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+}
