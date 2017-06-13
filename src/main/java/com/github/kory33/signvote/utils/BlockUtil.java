c25827d2c362add16b4243f782f484b1cdc4fec5
Kory33 <konryoitdv@gmail.com>
2017-06-13 18:30:33 +0200

    Amend BlockUtil algorithm

--- src/main/java/com/github/kory33/signvote/utils/BlockUtil.java
+++ src/main/java/com/github/kory33/signvote/utils/BlockUtil.java
@@ -3,26 +3,26 @@ package com.github.kory33.signvote.utils;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * An util class that handles things related to Blocks
  */
 public class BlockUtil {
+    private static final Stream<BlockFace> ADJACENT_FACE =
+            Stream.of(BlockFace.values()).filter(face -> Stream.of(face.getModX(), face.getModY(), face.getModZ())
+                            .mapToInt(Math::abs)
+                            .sum() == 1
+            );
+
     /**
      * Get a set of blocks whose face is touching to the given block
      * @param block target block
      * @return set of blocks whose face is touching to the given block
      */
     public static Set<Block> getBlocksAdjacentTo(Block block) {
-        Set<Block> blockSet = new HashSet<>();
-        blockSet.add(block.getRelative(BlockFace.DOWN));
-        blockSet.add(block.getRelative(BlockFace.UP));
-        blockSet.add(block.getRelative(BlockFace.NORTH));
-        blockSet.add(block.getRelative(BlockFace.EAST));
-        blockSet.add(block.getRelative(BlockFace.WEST));
-        blockSet.add(block.getRelative(BlockFace.SOUTH));
-        return blockSet;
+        return ADJACENT_FACE.map(block::getRelative).collect(Collectors.toSet());
     }
 }
