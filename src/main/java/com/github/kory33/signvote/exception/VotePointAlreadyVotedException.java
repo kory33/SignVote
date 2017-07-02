a76aaafdb892449dd2167f4cf46b1ecf98544ac0
Kory33 <konryoitdv@gmail.com>
2017-07-02 12:21:34 +0800

    Optimise exception classes

--- src/main/java/com/github/kory33/signvote/exception/VotePointAlreadyVotedException.java
+++ src/main/java/com/github/kory33/signvote/exception/VotePointAlreadyVotedException.java
@@ -1,35 +1,18 @@
 package com.github.kory33.signvote.exception;
 
-import java.util.UUID;
-
-import org.bukkit.entity.Player;
-
 import com.github.kory33.signvote.model.VotePoint;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 
-import lombok.Getter;
+import java.util.UUID;
 
 /**
  * Represents an exception thrown when a player attempts to vote to a vote point
  * which has already been voted by the same player.
  */
+@EqualsAndHashCode(callSuper = false)
+@Value
 public class VotePointAlreadyVotedException extends Exception {
-    @Getter UUID voterUUID;
-    @Getter VotePoint votePoint;
-
-    /**
-     * @deprecated Use {@link #VotePointAlreadyVotedException(UUID, VotePoint)} instead.
-     * @param voter a player who has attempted to vote
-     * @param votePoint a vote point to which a vote attempt was made
-     */
-    public VotePointAlreadyVotedException(Player voter, VotePoint votePoint) {
-        this.voterUUID = voter.getUniqueId();
-        this.votePoint = votePoint;
-    }
-
-    public VotePointAlreadyVotedException(UUID voterUUID, VotePoint votePoint) {
-        this.voterUUID = voterUUID;
-        this.votePoint = votePoint;
-    }
-
-    private static final long serialVersionUID = 7563600666472049099L;
+    UUID voterUUID;
+    VotePoint votePoint;
 }
